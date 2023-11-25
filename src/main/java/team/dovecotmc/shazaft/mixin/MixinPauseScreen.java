8b7b089efd57ddb6c5ef24673f536fa004ad8055
MikhailTapio <mikhailtapio13468@163.com>
2023-11-25 18:41:23 +0800

    Port to Fabric 1.20.1

--- src/main/java/team/dovecotmc/shazaft/mixin/MixinPauseScreen.java
+++ src/main/java/team/dovecotmc/shazaft/mixin/MixinPauseScreen.java
@@ -1,7 +1,7 @@
 package team.dovecotmc.shazaft.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.sounds.SoundInstance;
@@ -22,17 +22,17 @@ public abstract class MixinPauseScreen extends Screen {
         super(component);
     }
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/PauseScreen;drawCenteredString(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;III)V"))
-    private void inject$render(PoseStack poseStack, int i, int j, float f, CallbackInfo ci) {
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"))
+    private void inject$render(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci) {
         final SoundInstance sound = ((AccessorMusicManager) Minecraft.getInstance().getMusicManager()).getCurrentMusic();
         if (sound == null) return;
         final ShazaftConfig cfg = ShazaftClient.getCfg();
         final int w = (int) (this.width * cfg.width_coefficient_screen_width + cfg.width_constant);
         final int h = (int) (this.height * cfg.height_coefficient_screen_height + cfg.height_constant);
         if (cfg.centered)
-            PauseScreen.drawCenteredString(poseStack, this.font, Component.translatable("record.nowPlaying",
+            guiGraphics.drawCenteredString(this.font, Component.translatable("record.nowPlaying",
                     Utilities.getMusicName(sound.getSound().getLocation().toString())), w, h, Color.CYAN.getRGB());
-        else PauseScreen.drawString(poseStack, this.font, Component.translatable("record.nowPlaying",
+        else guiGraphics.drawString(this.font, Component.translatable("record.nowPlaying",
                 Utilities.getMusicName(sound.getSound().getLocation().toString())), w, h, Color.CYAN.getRGB());
     }
 }
