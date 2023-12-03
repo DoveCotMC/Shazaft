1167075ea90d46e51ff8c3420ffb27c820e8921c
MikhailTapio <mikhailtapio13468@163.com>
2023-12-03 14:53:14 +0800

    Enhance

--- src/main/java/team/dovecotmc/shazaft/config/ShazaftConfig.java
+++ src/main/java/team/dovecotmc/shazaft/config/ShazaftConfig.java
@@ -2,22 +2,54 @@ package team.dovecotmc.shazaft.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.awt.*;
+
 public class ShazaftConfig {
     public static final ForgeConfigSpec CFG;
-    public static final ForgeConfigSpec.BooleanValue centered;
-    public static final ForgeConfigSpec.DoubleValue height_coefficient_screen_height;
-    public static final ForgeConfigSpec.IntValue height_constant;
-    public static final ForgeConfigSpec.DoubleValue width_coefficient_screen_width;
-    public static final ForgeConfigSpec.IntValue width_constant;
+    public static final ForgeConfigSpec.BooleanValue pause_enabled;
+    public static final ForgeConfigSpec.BooleanValue pause_centered;
+    public static final ForgeConfigSpec.IntValue pause_color;
+    public static final ForgeConfigSpec.DoubleValue pause_height_coefficient_screen_height;
+    public static final ForgeConfigSpec.IntValue pause_height_constant;
+    public static final ForgeConfigSpec.DoubleValue pause_width_coefficient_screen_width;
+    public static final ForgeConfigSpec.IntValue pause_width_constant;
+    public static final ForgeConfigSpec.BooleanValue title_enabled;
+    public static final ForgeConfigSpec.BooleanValue title_centered;
+    public static final ForgeConfigSpec.IntValue title_color;
+    public static final ForgeConfigSpec.DoubleValue title_height_coefficient_screen_height;
+    public static final ForgeConfigSpec.IntValue title_height_constant;
+    public static final ForgeConfigSpec.DoubleValue title_width_coefficient_screen_width;
+    public static final ForgeConfigSpec.IntValue title_width_constant;
 
     static {
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        builder.push("display");
-        centered = builder.define("centered", false);
-        height_coefficient_screen_height = builder.defineInRange("height_coefficient_screen_height", 1.0, 0.0, 1.0);
-        height_constant = builder.defineInRange("height_constant", -15, -32768, 32767);
-        width_coefficient_screen_width = builder.defineInRange("width_coefficient_screen_width", 0.0, 0.0, 1.0);
-        width_constant = builder.defineInRange("width_constant", 5, -32768, 32767);
+        builder.push("Display");
+        builder.push("Pause");
+        pause_enabled = builder.define("pause_enabled", true);
+        pause_centered = builder.define("pause_centered", false);
+        pause_color = builder.defineInRange("pause_color", Color.CYAN.getRGB(), Color.BLACK.getRGB(), Color.WHITE.getRGB());
+        pause_height_coefficient_screen_height = builder
+                .defineInRange("pause_height_coefficient_screen_height", 1.0, 0.0, 1.0);
+        pause_height_constant = builder
+                .defineInRange("pause_height_constant", -15, -32768, 32767);
+        pause_width_coefficient_screen_width = builder
+                .defineInRange("pause_width_coefficient_screen_width", 0.0, 0.0, 1.0);
+        pause_width_constant = builder
+                .defineInRange("pause_width_constant", 2, -32768, 32767);
+        builder.pop();
+        builder.push("Title");
+        title_enabled = builder.define("title_enabled", true);
+        title_centered = builder.define("title_centered", false);
+        title_color = builder.defineInRange("title_color", Color.CYAN.getRGB(), Color.BLACK.getRGB(), Color.WHITE.getRGB());
+        title_height_coefficient_screen_height = builder
+                .defineInRange("title_height_coefficient_screen_height", 0.0, 0.0, 1.0);
+        title_height_constant = builder
+                .defineInRange("title_height_constant", 5, -32768, 32767);
+        title_width_coefficient_screen_width = builder
+                .defineInRange("title_width_coefficient_screen_width", 0.0, 0.0, 1.0);
+        title_width_constant = builder
+                .defineInRange("title_width_constant", 2, -32768, 32767);
+        builder.pop();
         builder.pop();
         CFG = builder.build();
     }
--- src/main/java/team/dovecotmc/shazaft/mixin/MixinTitleScreen.java
+++ src/main/java/team/dovecotmc/shazaft/mixin/MixinTitleScreen.java
@@ -2,8 +2,8 @@ package team.dovecotmc.shazaft.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,24 +16,24 @@ import java.awt.*;
 
 import static team.dovecotmc.shazaft.config.ShazaftConfig.*;
 
-@Mixin(PauseScreen.class)
-public abstract class MixinPauseScreen extends Screen {
-    protected MixinPauseScreen(Component title) {
+@Mixin(TitleScreen.class)
+public abstract class MixinTitleScreen extends Screen {
+    protected MixinTitleScreen(Component title) {
         super(title);
     }
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"))
+    @Inject(method = "render", at = @At("TAIL"))
     private void inject$render(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci) {
-        if (!pause_enabled.get()) return;
+        if (!title_enabled.get()) return;
         final SoundInstance sound = ((AccessorMusicManager) Minecraft.getInstance().getMusicManager()).getCurrentMusic();
         if (sound == null) return;
-        final int w = (int) (this.width * pause_width_coefficient_screen_width.get() +
-                pause_width_constant.get());
-        final int h = (int) (this.height * pause_height_coefficient_screen_height.get() +
-                pause_height_constant.get());
-        if (pause_centered.get())
+        final int w = (int) (this.width * title_width_coefficient_screen_width.get() +
+                title_width_constant.get());
+        final int h = (int) (this.height * title_height_coefficient_screen_height.get() +
+                title_height_constant.get());
+        if (title_centered.get())
             guiGraphics.drawCenteredString(this.font, Component.translatable("record.nowPlaying",
-                    Utilities.getMusicName(sound.getSound().getLocation().toString())), w, h, pause_color.get());
+                    Utilities.getMusicName(sound.getSound().getLocation().toString())), w, h, title_color.get());
         else guiGraphics.drawString(this.font, Component.translatable("record.nowPlaying",
                 Utilities.getMusicName(sound.getSound().getLocation().toString())), w, h, Color.CYAN.getRGB());
     }
